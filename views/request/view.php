431db25a05bec2c70c41d0b4131136aa5f2d9d1c
renba <cs_abner@hotmail.com>
2016-02-15 10:41:48 -0600

    *Cambio Correctivo *Descripcion: Se corrigio que se grabe la solicitud con el estado -Nuevo- y que se guardara con la fecha de creacion, tambien se agrego el boton de AdvanceOptions

--- views/request/view.php
+++ views/request/view.php
@@ -1,12 +1,21 @@
 <?php
-
+use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\request */
+
 ?>
 <div class="request-view">
- 
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+
+        <?= Html::a(Yii::t('app', 'Advanced options'), ['advanced-options', 'id' => $model->id],
+            ['class' => 'btn btn-primary pull-right']) ?>
+    </p>
+
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
