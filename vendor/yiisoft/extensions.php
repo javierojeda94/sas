1c8c4197ed343f5b5df0283dc3c922aa96bd15a1
renba <cs_abner@hotmail.com>
2016-02-21 23:39:34 -0600

    Cambio Nuevo, Calendarizaciòn de Solicitudes, Requisito RF17

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -122,4 +122,13 @@ return array (
     ),
     'bootstrap' => 'johnitvn\\rbacplus\\Bootstrap',
   ),
+  'yiisoft/yii2-jui' => 
+  array (
+    'name' => 'yiisoft/yii2-jui',
+    'version' => '2.0.0.0',
+    'alias' => 
+    array (
+      '@yii/jui' => $vendorDir . '/yiisoft/yii2-jui',
+    ),
+  ),
 );
